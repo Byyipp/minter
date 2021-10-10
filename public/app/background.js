dcfe9464ded27d22d406baa1a35c1ff460433bde
xBossBoyx <byip2001@gmail.com>
2021-10-10 03:12:33 -0400

    update

--- public/app/background.js
+++ public/app/background.js
@@ -1,6 +1,7 @@
 var mutationObserver = new MutationObserver(function(mutations) {
     mutations.forEach(function(mutation) {
         mint()
+        mint2()
         //console.log(mutation) //look at changes
     });
 });
@@ -10,28 +11,50 @@ var config2 = { characterData: true, attributes: false, childList: true, subtree
 
 
 (async() => {
+    //await(connect())
     //await connectPhantom()
     mutationObserver.observe(document.documentElement, config1); //innerhtml
     mutationObserver.observe(document.documentElement, config2); //textcontent
     mint()
+    mint2()
 })()
 
 
 async function mint() {
-    //while (true) {
         const button = document.getElementsByTagName("button")
         for (let i = 0; i < button.length; i++) {
             if (button[i].id.toLowerCase().includes("mint") == true || button[i].innerHTML.toLowerCase().includes("mint") == true || button[i].textContent.toLowerCase().includes("mint") == true){ //change whenever
-                //console.log("clicking")
                 for (let j = 0; j < 10; j++) {
                     button[i].click();
                 }
-
-                return;
             }
         }
-        //sleep(500)
-    //}
+        return;
+}
+
+async function mint2() {
+    console.log("mint2")
+    let elems = document.querySelectorAll('[class*="button" i]');
+
+    for (let i = 0; i < elems.length; i++) {
+        if ((elems[i].innerHTML.toLowerCase().includes("mint") || elems[i].textContent.toLowerCase().includes("mint")) && elems[i].innerHTML.toLowerCase().includes("how") == false && elems[i].textContent.toLowerCase().includes("how") == false)  {
+            for (let j = 0; j < 10; j++) {
+                elems[i].click()
+            }
+        }
+    }
+
+
+}
+
+async function connect() {
+    const injectedCode = `
+    solana.connect()
+    `
+
+    var script = document.createElement("script");
+    script.textContent = injectedCode;
+    (document.head).appendChild(script);
 }
 
 async function connectPhantom() {
@@ -44,8 +67,11 @@ async function connectPhantom() {
                 for (let j = 0; j < phantom.length; j++) {
                     if (phantom[j].className.includes("MuiButtonBase-root MuiButton-root MuiButton-text") == true) {
                         phantom[j].click()
+                        // var popup = chrome.extension.getViews({type: 'notification'})[0];
+                        // console.log(popup.Attr);
                     }
                 }
+            }
 
 
                 // let checkmark = document.getElementsByTagName("input")
@@ -63,8 +89,8 @@ async function connectPhantom() {
                 // }
 
                 return;
-            }
         }
+
 }
 
 function sleep(milliseconds) {
